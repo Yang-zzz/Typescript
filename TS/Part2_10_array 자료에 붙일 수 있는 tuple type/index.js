4bfaf57ddc1812ebfac33a66d1042d8408cbf835
Yangjisung <ygji6406@gmail.com>
2023-01-30 20:48:48 +0900

    Array 자료에 붙일 수 있는 tuple type

---
+++ TS/Part2_10_array 자료에 붙일 수 있는 tuple type/index.js
@@ -0,0 +1,61 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var 멍멍 = ["dog", true];
+var 멍멍2 = ["dog", true]; // tuple type 자료순서까지 타입지정 할때 사용
+var 멍멍3 = ["dog", true]; // 옵션기능도 가능
+var 멍멍4 = ["dog", true]; // 마지막에 넣어야함.
+function 테스트함수() {
+    var x = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        x[_i] = arguments[_i];
+    }
+}
+테스트함수(1, "222");
+var testArr = [1, 2, 3];
+var testArr2 = __spreadArray([4, 5], testArr, true);
+// 숙제1
+var 최근사먹은음식 = ["타코", 4000, true];
+// 숙제2
+var testarr = [
+    "동서녹차",
+    4000,
+    true,
+    false,
+    true,
+    true,
+    false,
+    true,
+];
+function 테스트함수2() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+}
+테스트함수2("a", true, 6, 3, "4", 4);
+// 숙제4
+function 분류기함수() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+    var result = [[], []];
+    rest.forEach(function (a) {
+        if (typeof a === "number") {
+            result[1].push(a);
+        }
+        else {
+            result[0].push(a);
+        }
+    });
+}
+분류기함수();
---
+++ TS/Part2_10_array 자료에 붙일 수 있는 tuple type/index.ts
@@ -0,0 +1,48 @@
+import { type } from "os";
+
+let 멍멍: (string | boolean)[] = ["dog", true];
+let 멍멍2: [string, boolean] = ["dog", true]; // tuple type 자료순서까지 타입지정 할때 사용
+let 멍멍3: [string, boolean?] = ["dog", true]; // 옵션기능도 가능
+let 멍멍4: [string, boolean?, number?] = ["dog", true]; // 마지막에 넣어야함.
+
+function 테스트함수(...x: [number, string]) {}
+테스트함수(1, "222");
+
+let testArr = [1, 2, 3];
+let testArr2: [number, number, ...number[]] = [4, 5, ...testArr];
+
+// 숙제1
+let 최근사먹은음식: [string, number, boolean] = ["타코", 4000, true];
+
+// 숙제2
+let testarr: [string, number, ...boolean[]] = [
+  "동서녹차",
+  4000,
+  true,
+  false,
+  true,
+  true,
+  false,
+  true,
+];
+
+// 숙제3
+type 테스트파라미터 = [string, boolean, ...(number | string)[]];
+function 테스트함수2(...rest: 테스트파라미터) {}
+테스트함수2("a", true, 6, 3, "4", 4);
+
+// 숙제4
+
+function 분류기함수(...rest: (number | string)[]) {
+  let result: [string[], number[]] = [[], []];
+
+  rest.forEach((a) => {
+    if (typeof a === "number") {
+      result[1].push(a);
+    } else {
+      result[0].push(a);
+    }
+  });
+}
+
+분류기함수();
