3a84d4c2f0561469db6dda2c854c3bef80b1c369
Yangjisung <ygji6406@gmail.com>
2023-02-03 00:55:57 +0900

    조건문으로 타입만들기 & infer

---
+++ TS/Part2_16_조건문으로 타입만들기 & infer/index.js
@@ -0,0 +1,9 @@
+// type if 문은 삼항연산자로
+// 조건식은 extends를 사용하여
+var test; // string
+var test2; // unknown
+var test3;
+var test4;
+var test5;
+var test6;
+var test7;
---
+++ TS/Part2_16_조건문으로 타입만들기 & infer/index.ts
@@ -0,0 +1,29 @@
+type Agetest<T> = T extends string ? string : unknown; // T 라는 파라미터가 string가 맞다면...
+// type if 문은 삼항연산자로
+// 조건식은 extends를 사용하여
+
+let test: Agetest<string>; // string
+let test2: Agetest<number>; // unknown
+
+//연습문제
+type FirstItem<T> = T extends any[] ? T[0] : any;
+let test3: FirstItem<string[]>;
+let test4: FirstItem<number>;
+
+// infet 키워드 : 조건문에서 쓸 수 있는 infer 키워드, 타입을 왼쪽에서 추출해줌.
+type TestPerson<T> = T extends infer R ? string : unknown;
+let test5: TestPerson<string>;
+// 일 때 R === string
+
+// infet 키워드2 : array 내부의 타입만 뽑고 싶을 때
+type 타입추출<T> = T extends (infer R)[] ? R : unknown;
+let test6: 타입추출<string[]>;
+// 일 때 R === stirng
+
+// infet 키워드3 : 함수를 넣으면 함수의 return 타입만 뽑고 싶다.
+type 타입추출2<T> = T extends () => infer R ? R : unknown;
+let test7: 타입추출2<() => void>;
+// 일 때 R === void
+
+// 근데 ReturnType 이라는 기본 함수가 있다.
+type b = ReturnType<() => void>
