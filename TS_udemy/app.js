4277c22170553a5e915158e1338825bc1b803528
Yangjisung <ygji6406@gmail.com>
2023-04-06 14:42:29 +0900

    TypeScript : Tupple Type

--- TS_udemy/app.js
+++ TS_udemy/app.js
@@ -1,24 +1,38 @@
 var person1 = {
-    name: "Maxin",
-    age: 30
+  name: "Maxin",
+  age: 30,
 };
-// person1 객체는 타입을 명시적으로 작성한 객체
 var person2 = {
-    name: "Maxin",
-    age: 30,
-    hobies: ['sport', 'Cooking'],
+  name: "Maxin",
+  age: 30,
+  hobies: ["sport", "Cooking"],
+  role: [2, "author"],
 };
 console.log(person2.name);
-// person2 객체는 key의 타입을 추론하였음.
+
+// 타입스크립트 튜플타입 예제
+person2.role.push("admin");
+person2.role.push(54);
+// person2.role[0] = 10 // o
+// person2.role[1] = 'YangZ' // o
+// person2.role[1] = 10; // x
+// person2.role[2] = 10; // x
+/**
+ * person2.role key value의 타입은 [number, string] tupple type으로 길이가 2이며 0번째와 1번째 인덱스에는 number, string 데이터만 적용될 수 있다.
+ * 하지만 push 메서드를 통해 주입된 'admin'과 54는 적용 가능하다. tupple type에 벗어나지 않았기 때문에 예외적으로 가능하다.
+ * 18,19 Lines 처럼 role key value에 직접 접근하여 데이터를 조작을 시도 하였으나, tupple type에 벗어난다는 typeScrip의 에러를 받을 수 있었다.
+ */
+
+// 타입스크립트의 타입추론 기능 예제
 for (var _i = 0, _a = person2.hobies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map())
+  var hobby = _a[_i];
+  console.log(hobby);
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map())
 }
 /**
  * TypeScript는 person2 객체 안에 있는 hobies key를 배열타입을 추론하였으며,
  * hobies배열 안에 있는 인자들 또한 string 타입을 추론하고 있음. 때문에 문자열에 사용할 수 있는 toUpperCase()가 적용되었으며,
  * map()메서드는 사용할 수 없기 때문에 오류를 띄어주고 있음.
  *
- * */ 
+ * */
--- TS_udemy/app.ts
+++ TS_udemy/app.ts
@@ -1,28 +1,43 @@
 const person1: {
-    name : string;
-    age : number;
+  name: string;
+  age: number;
 } = {
-    name : "Maxin",
-    age : 30
-}
-// person1 객체는 타입을 명시적으로 작성한 객체
-
-const person2 = {
   name: "Maxin",
   age: 30,
-  hobies: ['sport', 'Cooking'],
+};
+
+const person2: {
+  name: string;
+  age: number;
+  hobies: string[];
+  role: [number, string]; // tupple type => 배열안에 0번째 index == number, 1번째 index == string인 특수배열
+} = {
+  name: "Maxin",
+  age: 30,
+  hobies: ["sport", "Cooking"],
+  role: [2, "author"],
 };
 console.log(person2.name);
-// person2 객체는 key의 타입을 추론하였음.
 
-for(const hobby of person2.hobies) {
-    console.log(hobby)
-    console.log(hobby.toUpperCase())
-    // console.log(hobby.map())
+// 타입스크립트 튜플타입 예제
+person2.role.push("admin");
+person2.role.push(54);
+console.log(person2.role);
+// person2.role[1] = 10;
+// person2.role[2] = 10;
+/**
+ * role key value의 타입은 [number, string] 튜플 타입으로 0번째와 1번째 인덱스에는 각각 해당하는 타입의 데이터만 들어올 수 있다. 하지만 push 메서드를 통해 주입된 데이터는 오류를 띄지 않는다.
+ */
+
+// 타입스크립트의 타입추론 기능 예제
+for (const hobby of person2.hobies) {
+  console.log(hobby);
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map())
 }
-/** 
+/**
  * TypeScript는 person2 객체 안에 있는 hobies key를 배열타입을 추론하였으며,
  * hobies배열 안에 있는 인자들 또한 string 타입을 추론하고 있음. 때문에 문자열에 사용할 수 있는 toUpperCase()가 적용되었으며,
  * map()메서드는 사용할 수 없기 때문에 오류를 띄어주고 있음.
- * 
- * */ 
+ *
+ * */
